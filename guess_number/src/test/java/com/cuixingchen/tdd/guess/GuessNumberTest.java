a6e70181b395dfdbc1123abbf590bbc58fd7367a
cuipengfei1 <cuipengfei1@jd.com>
2018-05-16 14:35:06 +0800

    初始化配置

---
+++ guess_number/src/test/java/com/cuixingchen/tdd/guess/GuessNumberTest.java
@@ -0,0 +1,8 @@
+package com.cuixingchen.tdd.guess;
+
+/**
+ * Created by cuipengfei1 on 2018/5/16.
+ */
+public class GuessNumberTest {
+
+}
---
+++ permutation/src/test/java/com/cuixingchen/tdd/permutation/PermutationTest.java
@@ -0,0 +1,7 @@
+package com.cuixingchen.tdd.permutation;
+
+/**
+ * Created by cuipengfei1 on 2018/5/16.
+ */
+public class PermutationTest {
+}
